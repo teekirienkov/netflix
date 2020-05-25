e4d3cad0ca30b7f1ed0fee1784726698f82115d5
Timur <teekirienkov@gmail.com>
2020-05-25 21:47:32 +0300

    add eventlistener for close leftmenu click document

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ hamburger.addEventListener('click', () => {
   leftMenu.classList.toggle('openMenu');
   hamburger.classList.toggle('open');
 })
+
+// Закрытие меню по клику в любую область страницы
+document.addEventListener('click', (event) => {
+  // Проверка на родительский класс левого меню, клик внутри меню
+  if (!event.target.closest('.left-menu')) {
+    leftMenu.classList.remove('openMenu');
+    hamburger.classList.remove('open');
+  }
+})
